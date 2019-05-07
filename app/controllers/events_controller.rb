b96972c644fe0c9bba1a88278b4811057cb41e2d
Hazrian <chazotflorian@gmail.com>
2019-05-07 18:33:49 +0200

    push heroku

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,22 @@
+class EventsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def new
+  end
+end
